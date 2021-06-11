8166bde4c3c86fed821549370e8fd771468a00bd
EC2 Default User <ec2-user@ip-172-31-37-84.us-east-2.compute.internal>
2021-06-11 01:02:55 +0000

    Use of policies in lambda functions, request body added on swagger

--- dynamo_api/src/user.py
+++ dynamo_api/src/user.py
@@ -10,9 +10,14 @@ table = dynamodb.Table(users_table)
 def getUser(event, context):
     print(json.dumps({"running": True}))
     print(json.dumps(event))
+    
+    path = event["path"]
+    array_path = path.split("/")
+    user_id = array_path[-1]
+    
     response = table.get_item(
         Key={
-            'pk': 'user_',
+            'pk': user_id,
             'sk': 'age'
         }
     )
@@ -20,19 +25,30 @@ def getUser(event, context):
     print(item)
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(item)
     }
 
 def putUser(event, context):
     print(json.dumps({"running": True}))
     print(json.dumps(event))
+    
+    path = event["path"]
+    array_path = path.split("/")
+    user_id = array_path[-1]
+    
+    body = event["body"]
+    body_object = json.loads(body)
+    
     table.put_item(
        Item={
-            'pk': 'user_',
-            'sk': 'age'
+            'pk': user_id,
+            'sk': 'age',
+            'name': body_object['name'],
+            'last_name': body_object['last_name'],
+            'age': body_object['age']
         }
     )
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Successfully inserted')
     }
